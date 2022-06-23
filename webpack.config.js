46e4ae132425e16773e715f77265773d5d555aca
すずねーう <suzuneu@suzuneu.com>
2022-06-23 03:36:08 +0000

    成功時

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+const debug = process.env.NODE_ENV !== "production";
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  entry: "./js/client.js",
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env']
+        }
+      }]
+    }]
+  },
+  mode: debug ? "development" : "production",
+  output: {
+    path: __dirname + "/src/",
+    filename: "client.min.js",
+//    publicPath: "/src/"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/src/')
+    }
+  },
+};
