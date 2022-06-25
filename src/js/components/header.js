b5c91ce421bd922e1f9c1857f759d4f0b1f3ec7d
すずねーう <suzuneu@suzuneu.com>
2022-06-25 03:01:51 +0000

    cmt

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -9,7 +9,7 @@ export default function Header() {
         <Container>
           <div className="navbar-header text-light">
             <NavbarBrand className="text-light">
-              <a href="/" style={{ display: "inline-flex", color: "white", "text-decoration": "none" }} id="brandbar">
+              <a href="/" style={{ display: "inline-flex", color: "white", textDecoration: "none" }} id="brandbar">
                 <img src="/assets/logo.png" width="70" />
                 <h1>Pastedeck</h1>
               </a>
